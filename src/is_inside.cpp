5928d07cc19614a74273467dd69b7ada0df92e6a
azzaakamoun <azza.kammoun@unil.ch>
2018-11-27 14:57:08 +0100

    add is_inside.cpp

---
+++ src/is_inside.cpp
@@ -0,0 +1,17 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+
+// [[Rcpp::export]]
+NumericMatrix is_inside(NumericMatrix points) {
+  LogicalVector inside(points.nrow());
+
+}
+
+
+
+
+/*** R
+timesTwo(42)
+*/
