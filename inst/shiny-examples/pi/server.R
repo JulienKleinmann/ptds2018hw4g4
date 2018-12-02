25fbf2c22a62ad6f408d07ada9f20ab4b2e11f9c
azzaakamoun <azza.kammoun@unil.ch>
2018-12-02 14:34:36 +0100

    added function in simulate

--- inst/shiny-examples/pi/server.R
+++ inst/shiny-examples/pi/server.R
@@ -1,35 +1,37 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
-library(ptds2018hw4g4) 
+library(ptds2018hw4g4)
 
 shinyServer(function(input, output) {
-  
+
   simulate <- reactive({
     # simulate pi and measure the time here
-    ...
+    if (input$method == "estimate_pi") {
+      estimate_pi(input$seed, input$B)
+    } else {estimate_pi2(input$seed, input$B)}
   })
-  
+
   output$plot <- renderPlot({
     # plot pi
     ...
   })
-  
+
   output$time <- renderText({
     # extract the time of the execution
     ...
   })
-  
+
   output$pi <- renderText({
     # extract the estimated value
     ...
   })
-  
+
 })
